8ea28e38dd8e354ed30da855e7398a34ec460c4e
Denis <dennydenii@gmail.com>
2024-02-02 12:42:12 +0000

    cenas

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,54 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useRef, useState } from "react";
+import styles from "./App.module.css";
 
+const messagesWithTimers = [
+  { text: "Hey boo", timer: 1000 },
+  { text: "Whats up girl?", timer: 1500 },
+  { text: "WHAAAAAATSSS UUUUUPPPPP", timer: 2000 },
+  { text: "sorry...", timer: 1000 },
+  { text: "I have an important question to ask you", timer: 2500 },
+  { text: "Do...", timer: 1000 },
+  { text: "Do...", timer: 1000 },
+  { text: "Do...", timer: 1000 },
+  { text: "BABY SHARK, DOO, DOO, DOO, DOO, DOO, DOO", timer: 3000 },
+  { text: "...", timer: 500 },
+  { text: "Seriously.", timer: 1000 },
+  { text: "Do you want to be my valentine?", timer: 3000 },
+];
 function App() {
-  const [count, setCount] = useState(0)
+  const [index, setIndex] = useState(0);
 
+  const showNextMessage = () => {
+    setIndex((ind) => ind++);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className={styles.background}></div>
+      <Text
+        text={messagesWithTimers[index].text}
+        onReachHeight={showNextMessage}
+      ></Text>
     </>
-  )
+  );
 }
 
-export default App
+const Text = ({
+  text,
+  onReachHeight,
+}: {
+  text: string;
+  onReachHeight: () => void;
+}) => {
+  const [position, setPosition] = useState(0);
+  useEffect(() => {
+    setInterval(() => {
+      if (position < 600) {
+        setPosition((pos) => pos++);
+      }
+    }, 10000);
+  }, []);
+
+  return <p style={{ position: "absolute", top: `${position}` }}>{text}</p>;
+};
+
+export default App;
